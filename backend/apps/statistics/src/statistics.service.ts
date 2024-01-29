806cb8fd5051509a806647f362e4a5b957db4c0c
mariskoch <maris@koch-24.de>
2024-01-29 22:03:04 +0100

    services tell who they are in hello world

--- backend/apps/statistics/src/statistics.service.ts
+++ backend/apps/statistics/src/statistics.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class StatisticsService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello statistics!';
   }
 }
