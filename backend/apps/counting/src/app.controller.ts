591ee8a54ea60aabc7f02682ffa661f781b95b29
Maris Koch <maris.koch@daimler.com>
2024-01-31 22:23:58 +0100

    first functions in counting

--- backend/apps/counting/src/app.controller.ts
+++ backend/apps/counting/src/app.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { AppService, Counter } from './app.service';
+import CreateCounterDto from './dtos/create-counter.dto';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/counters')
+  getCounters(): Counter[] {
+    return this.appService.getCounters();
+  }
+
+  @Post('/create')
+  createCounter(@Body() createCounter: CreateCounterDto): { name: string } {
+    return this.appService.createCounter(createCounter);
   }
 }
--- backend/apps/counting/src/app.service.ts
+++ backend/apps/counting/src/app.service.ts
@@ -1,8 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import CreateCounterDto from './dtos/create-counter.dto';
+
+export interface Counter {
+  name: string;
+  count: number;
+}
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello from counting!';
+  private counters: Counter[] = [];
+
+  createCounter(createCounter: CreateCounterDto): { name: string } {
+    if (this.doesCounterAlreadyExistByName(createCounter.name)) {
+      throw new ConflictException(
+        `Counter with name ${createCounter.name} already exists`,
+      );
+    }
+    this.counters.push({ name: createCounter.name, count: 0 });
+    return { name: createCounter.name };
+  }
+
+  getCounters(): Counter[] {
+    return this.counters;
+  }
+
+  private doesCounterAlreadyExistByName(name: string): boolean {
+    return this.counters.some((counter) => counter.name === name);
   }
 }
