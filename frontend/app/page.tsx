81ff3abfa56bc37978566b33dc70788c78d56d2b
Maris Koch <maris.koch@daimler.com>
2024-02-01 17:03:45 +0100

    Further work on Counter page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,9 +4,11 @@ export default function Home() {
     return (
         <div className={'flex justify-center'}>
             <Container classes={'mt-24'}>
-                Hello World!
+                <h1 className={'text-3xl font-bold'}>Counters</h1>
+                <button className={'border-solid border-2 border-gray-800 rounded-md px-3 ml-auto hover:bg-gray-800 ' +
+                    'hover:text-gray-200 transition duration-200'}>New Counter
+                </button>
             </Container>
         </div>
-
     );
 }
