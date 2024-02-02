6f9f8eb3037aca7629e88c9799bcc833fc845fe7
Maris Koch <maris.koch@daimler.com>
2024-02-02 22:14:07 +0100

    first working modal

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,15 +1,21 @@
 import Container from "@/components/Container";
+import Link from "next/link";
+import Modal from "@/components/Modal";
 
-export default function Home() {
+export default function Home({searchParams}: { searchParams: { [key: string]: string | string[] | undefined } }) {
+    const show = searchParams?.show;
+
     return (
         <div className={'flex justify-center'}>
             <Container classes={'mt-24'}>
                 <h1 className={'text-3xl font-bold'}>Counters</h1>
-                <button className={'border-solid border-2 border-gray-800 rounded-md px-3 ml-auto hover:bg-gray-800 ' +
-                    'hover:text-gray-200 transition duration-200'}>New Counter
-                </button>
+                <Link href={'/?show=true'}
+                      className={'border-solid border-2 border-gray-800 rounded-md px-3 ml-auto hover:bg-gray-800 ' +
+                          'hover:text-gray-200 transition duration-200 flex items-center'}>New Counter
+                </Link>
             </Container>
+
+            {show && (<Modal show={Boolean(show)}></Modal>)}
         </div>
     );
 }
-
