9389b4d05659efbf07e5324df8b9bc5441ea92b8
mariskoch <maris@koch-24.de>
2024-02-04 15:20:55 +0100

    add todo

--- frontend/app/api/submit/route.ts
+++ frontend/app/api/submit/route.ts
@@ -1,5 +1,6 @@
 export async function POST (req: Request) {
     const data = await req.json();
+    // TODO: add body validation
     console.log(data);
     return Response.json({ message: 'Hello, World!' });
 }
